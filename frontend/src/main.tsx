b41346578fe6012e17276b9c880c1f1dc8bc4103
YesMyNameIsMe <nguyen424787@gmail.com>
2026-02-21 17:42:22 -0600

    login fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
-import LandingPage from './LandingPage.tsx'
+import Login from './login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <StrictMode>
       <Routes>
         <Route path="/" element={<App/>}/>
-        <Route path="/login" element={<LandingPage/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
     </StrictMode>
   </BrowserRouter>
